5f4f351bdafba0b269142ab94ac4dbf0d62c1c65
Kevin <kevin.hercog@davinci.edu.ar>
2023-06-18 21:25:05 -0300

    Intro UI almost done

--- script.js
+++ script.js
@@ -2,78 +2,66 @@ function Game()
 {
     let psTotal = 0;
     let csTotal = 0;
+    let ready = null;
 
-    for (i = 0; i < 5; i++)
-    {
+    if (ready != null) {
+
         let playerSelection = prompt('Select rock, paper or scissors please.');
 
         let computerSelection = getComputerChoice();
 
         playRound(playerSelection, computerSelection);
 
-        function getComputerChoice()
-        {
-            randNumber = Math.floor(Math.random() * 3 + 1 );
+        function getComputerChoice() {
+            randNumber = Math.floor(Math.random() * 3 + 1);
 
-            if (randNumber == 1)
-            {
+            if (randNumber == 1) {
                 console.log("rock");
                 return "rock";
-            } else if (randNumber == 2)
-            {
+            } else if (randNumber == 2) {
                 console.log("paper");
                 return "paper";
-            } else if (randNumber == 3)
-            {
+            } else if (randNumber == 3) {
                 console.log("scissors");
                 return "scissors";
             }
-
         }
 
-        function playRound(playerSelection, computerSelection)
-        {
+
+        function playRound(playerSelection, computerSelection) {
             let ps = playerSelection.toLowerCase();
             let cs = computerSelection.toLowerCase();
 
-            if (ps === cs)
-            {
+            if (ps === cs) {
                 console.log('Draw!');
-            }
-            else if (ps === 'rock' && cs === 'paper'||
+            } else if (ps === 'rock' && cs === 'paper' ||
                 ps === 'paper' && cs === 'scissors' ||
-                ps === 'scissors' && cs === 'rock')
-            {
+                ps === 'scissors' && cs === 'rock') {
                 csTotal++;
                 console.log('Player looses!');
-            }
-            else if (ps === 'rock' && cs === 'scissors' ||
+            } else if (ps === 'rock' && cs === 'scissors' ||
                 ps === 'paper' && cs === 'rock' ||
-                ps === 'scissors' && cs === 'paper')
-            {
+                ps === 'scissors' && cs === 'paper') {
                 psTotal++;
                 console.log('Player wins!');
-            }
-            else
-            {
+            } else {
                 return 'Something went wrong! Please refresh the website';
             }
         }
-    }
 
-    if (psTotal > csTotal)
-    {
-        return console.log('Player wins!\n' +
-            'Player: ' + psTotal + '\n' +
-            'PC: ' + csTotal);
-    } else if (psTotal < csTotal )
-    {
-        return console.log('Computer wins!\n' +
-            'PC: ' + csTotal + '\n' +
-            'Player: ' + psTotal );
-    } else if (psTotal === csTotal)
-    {
-        return console.log("it's a Draw");
+
+        if (psTotal > csTotal) {
+            return console.log('Player wins!\n' +
+                'Player: ' + psTotal + '\n' +
+                'PC: ' + csTotal);
+        } else if (psTotal < csTotal) {
+            return console.log('Computer wins!\n' +
+                'PC: ' + csTotal + '\n' +
+                'Player: ' + psTotal);
+        } else if (psTotal === csTotal) {
+            return console.log("it's a Draw");
+        }
+
     }
 }
 
